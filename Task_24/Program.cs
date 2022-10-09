59c160605710463dfd2f8dd5506a76cea8e86ca0
Sergey <motor-person@yandex.ru>
2022-10-09 11:58:15 +0400

    Решили Task_24

--- Task_24/Program.cs
+++ Task_24/Program.cs
@@ -1,1 +1,21 @@
-﻿Console.Clear();
+﻿
+
+// Напишите программму, которая принимает на вход число (А) и выдает сумму чисел от 1 до (А)
+
+Console.Clear();
+
+
+System.Console.WriteLine("Пожалуйста, введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumNumbers(int num)
+{
+    int sum = default;  // default - исходное хначение (0)
+    for (int i = 1; i <= num; i++)
+    {
+        sum = sum + i;
+    }
+    return sum;
+}
+int result = SumNumbers(number);
+Console.WriteLine($"Сумма чисел от 1 до {number} равна {result}");
