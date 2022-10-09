c36807eadd76eaa7bc27506f332b774abfe1c03e
Sergey <motor-person@yandex.ru>
2022-10-09 15:52:49 +0400

    Добавили Task_25

--- Task_30/Program.cs
+++ Task_30/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// // Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном поряжке
+Console.Clear();
+
+int[] arr = new int[8];
+
+
+void FillArray(int[] ar)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < ar.Length; i++)
+    {
+        ar[i] = rnd.Next(0, 2);
+    }
+}
+
+FillArray(arr);
+
+ void PrintArray(int[] ar)
+{
+    int count = ar.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write ($"{ar[position]}, ");
+        position++;
+    }
+}
+
+PrintArray(arr);
